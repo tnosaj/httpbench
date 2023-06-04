1314c82b588456273563046694a9b0f861aa2f79
Jason Tevnan <jason.tevnan@gmail.com>
2023-06-04 21:22:02 +0200

    use timeout context

--- strategy/simple/simple.go
+++ strategy/simple/simple.go
@@ -1,8 +1,10 @@
 package simple
 
 import (
+	"context"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
@@ -25,7 +27,21 @@ func (st SimpleStrategy) RunCommand() {
 
 func (st SimpleStrategy) curl() {
 	timer := prometheus.NewTimer(st.S.Metrics.RequestDuration)
-	res, err := http.Get(st.S.Url)
+
+	req, err := http.NewRequest("GET", st.S.Url, nil)
+	if err != nil {
+		st.S.Metrics.ErrorRequests.WithLabelValues("create").Inc()
+		logrus.Errorf("Error creating url: %s - %s", st.S.Url, err)
+	}
+
+	ctx, cancel := context.WithTimeout(req.Context(), time.Duration(st.S.Timeout)*time.Second)
+	defer cancel()
+
+	req = req.WithContext(ctx)
+
+	client := http.DefaultClient
+	res, err := client.Do(req)
+
 	if err != nil {
 		st.S.Metrics.ErrorRequests.WithLabelValues(strconv.Itoa(res.StatusCode)).Inc()
 		logrus.Errorf("Error getting url: %s - %s", st.S.Url, err)
